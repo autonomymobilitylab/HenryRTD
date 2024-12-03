bcbf6e3634399f4fd2834832e7fa3a49442139b5
Eelis Peltola <eelis.peltola@aalto.fi>
2024-12-03 11:01:35 +0200

    Remove version info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'Henry'
 copyright = '2024, Autonomy and Mobility Lab'
 author = 'Eelis Peltola'
 
-release = '0.1'
-version = '0.1.0'
+# release = '1.0'
+# version = '1.0.0'
 
 # -- General configuration
 
